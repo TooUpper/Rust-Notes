cb5717b4cd13fa389ea9bb68dc8952a786903472
kay <133548209+TooUpper@users.noreply.github.com>
2023-09-17 04:23:22 +0800

    Delete learning-Rust/Rust-Basic/test1 directory

--- learning-Rust/Rust-Basic/test1/target/debug/deps/test1-2cd62f041f2619f3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\cnkay\Desktop\Rust-Notes\learning-Rust\Rust-Basic\test1\target\debug\deps\test1-2cd62f041f2619f3.rmeta: src\main.rs
-
-C:\Users\cnkay\Desktop\Rust-Notes\learning-Rust\Rust-Basic\test1\target\debug\deps\test1-2cd62f041f2619f3.d: src\main.rs
-
-src\main.rs:
--- learning-Rust/Rust-Basic/test1/target/debug/deps/test1-a421523481c49ef8.d
+++
@@ -1,5 +0,0 @@
-C:\Users\cnkay\Desktop\Rust-Notes\learning-Rust\Rust-Basic\test1\target\debug\deps\test1-a421523481c49ef8.rmeta: src\main.rs
-
-C:\Users\cnkay\Desktop\Rust-Notes\learning-Rust\Rust-Basic\test1\target\debug\deps\test1-a421523481c49ef8.d: src\main.rs
-
-src\main.rs:
